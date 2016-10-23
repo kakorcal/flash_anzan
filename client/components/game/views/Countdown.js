e8db04ab260c78740fac17adfab9a5a4ad1fe7de
Kenneth Korcal <kakorcal@gmail.com>
2016-10-22 21:01:59 -0700

    add countdown view

--- client/components/game/views/Countdown.js
+++ client/components/game/views/Countdown.js
@@ -1,10 +1,54 @@
 import React, {Component} from 'react'
 
 class Countdown extends Component{
-  render(){
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 3,
+      timerId: null
+    };
+  }
+
+  changeView(view){
+    this.props.changeView(view);
+  }
+
+  createCircles(){
+    const circles = [];
+    for(var i = this.state.count; i >= 1; i--){
+      circles.push(
+        <i key={i} className='fa fa-circle fa-3x flash-co-yellow'></i>
+      );
+    }
     return (
-      <div>
-        Countdown
+      <div className='circles'>
+        {circles}
+      </div>
+    );
+  }
+
+  componentWillMount(){
+    this.startTimer();
+  }
+
+  startTimer(){
+    var timerId = window.setInterval(() => {
+      var count = this.state.count - 1;
+      if(!~count) this.changeView('play');
+      else this.setState({count});
+    }, 1000);
+    this.setState({timerId});
+  }
+
+  componentWillUnmount(){
+    window.clearInterval(this.state.timerId);
+  }
+
+  render(){
+    const circleWrapper = this.createCircles();
+    return (
+      <div className='countdown'>
+        {circleWrapper}
       </div>
     );
   }
