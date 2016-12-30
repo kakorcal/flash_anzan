aa8a8b45e1ee7472154b335126a630c04e1f635b
Kenneth Korcal <kakorcal@gmail.com>
2016-12-31 00:09:09 +0900

    setup heroku mlabs

--- server/config/env.js
+++ server/config/env.js
@@ -1,6 +1,6 @@
 if(!process.env.NODE_ENV) require('dotenv').load();
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const PORT = process.env.PORT || 3000;
-export const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/flash_anzan';
+export const DATABASE_URL = process.env.MONGODB_URI || 'mongodb://localhost/flash_anzan';
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const X_MASHAPE_KEY = process.env.X_MASHAPE_KEY;
